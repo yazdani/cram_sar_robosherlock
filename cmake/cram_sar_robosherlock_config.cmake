803823c04b8967d514fa002a5ceff8f4f5e0d980
Fereshta Yazdani <yazdani@cs.uni-bremen.de>
2016-07-20 10:44:56 +0200

    added new files

---
+++ cmake/cram_sar_robosherlock_config.cmake
@@ -0,0 +1,6 @@
+set(cram_sar_robosherlock_NAMESPACE           cram_sar_robosherlock)
+set(cram_sar_robosherlock_TYPESYSTEM_CPP_PATH /home/yazdani/work/ros/indigo/catkin_ws/src/cram_sar_robosherlock/include/cram_sar_robosherlock/types)
+set(cram_sar_robosherlock_TYPESYSTEM_XML_PATH /home/yazdani/work/ros/indigo/catkin_ws/src/cram_sar_robosherlock/descriptors/typesystem)
+set(cram_sar_robosherlock_ANNOTATOR_PATH      /home/yazdani/work/ros/indigo/catkin_ws/src/cram_sar_robosherlock/descriptors/annotators)
+set(cram_sar_robosherlock_ENGINE_PATH         /home/yazdani/work/ros/indigo/catkin_ws/src/cram_sar_robosherlock/descriptors/analysis_engines)
+
